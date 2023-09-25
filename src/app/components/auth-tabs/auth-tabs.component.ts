01f3048262d6da599c2d81f9e61f6bfcadf391b7
Charity Smith <charityasmith@gmail.com>
2023-09-24 22:19:00 -0600

    Add files via upload

--- src/app/components/auth-tabs/auth-tabs.component.ts
+++ src/app/components/auth-tabs/auth-tabs.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-auth-tabs',
   templateUrl: './auth-tabs.component.html',
-  styleUrls: ['./auth-tabs.component.css']
+  styleUrls: ['./auth-tabs.component.css'],
 })
-export class AuthTabsComponent {
+export class AuthTabsComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit() {
+    const tabs = document.querySelector('.tabs');
+    if (tabs !== null) {
+      M.Tabs.init(tabs, {});
+    }
+  }
 }
--- src/app/modules/auth.module.ts
+++ src/app/modules/auth.module.ts
@@ -1,10 +1,12 @@
 import { AuthTabsComponent } from '../components/auth-tabs/auth-tabs.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from '../components/login/login.component';
+import { SignupComponent } from '../components/signup/signup.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [AuthTabsComponent],
-  exports: [AuthTabsComponent],
+  declarations: [AuthTabsComponent, LoginComponent, SignupComponent],
+  exports: [AuthTabsComponent, LoginComponent, SignupComponent],
 })
 export class AuthModule {}
